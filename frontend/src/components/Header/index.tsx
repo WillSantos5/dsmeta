741fdb8ad91d1b591921539f21235444db0e6d6f
WillSantos5 <wmsantos148@gmail.com>
2022-07-12 17:40:47 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvedor | 
+                    <a href="https://github.com/WillSantos5"> GitHub: WillSantos5</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
+
